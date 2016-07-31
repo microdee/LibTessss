401a3419709aa779592f5dea785127d7be0e2308
David Morasz <m@mcro.de>
2016-07-31 15:26:00 +0200

    Nodes are ready for testing

---
+++ src/LibTessss/Nodes.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibTessDotNet;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VMath;
+
+namespace LibTessss.Nodes
+{
+    [PluginInfo(
+        Name = "Contours",
+        Category = "LibTessss",
+        Author = "microdee",
+        Help = "Prepare contours for LibTessss",
+        Tags = "Triangulate, Polygons",
+        AutoEvaluate = true
+    )]
+    public class LibTessssContourNode : IPluginEvaluate
+    {
+        [Input("Points ")]
+        public ISpread<ISpread<Vector2D>> FIn;
+
+        [Input("Orientation", DefaultEnumEntry = "Original")]
+        public ISpread<ContourOrientation> FOrientation;
+
+        [Output("Output")]
+        public ISpread<VContour> FOut;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (FIn.IsChanged)
+            {
+                FOut.SliceCount = FIn.SliceCount;
+                for (int i = 0; i < FIn.SliceCount; i++)
+                {
+                    var cnt = new VContour {
+                        Contour = new ContourVertex[FIn[i].SliceCount],
+                        Orientation = FOrientation[i]
+                    };
+                    for (int j = 0; j < FIn[i].SliceCount; j++)
+                    {
+                        cnt[j] = new ContourVertex {
+                            Position = new Vec3 {
+                                X = (float) FIn[i][j].x,
+                                Y = (float) FIn[i][j].y
+                            }
+                        };
+                    }
+                    FOut[i] = cnt;
+                }
+            }
+        }
+    }
+
+    [PluginInfo(
+        Name = "LibTessss",
+        Category = "2d",
+        Author = "microdee",
+        Help = "Triangulate using LibTessDotNet",
+        Tags = "Triangulate, Polygons"
+    )]
+    public class LibTessssNode : IPluginEvaluate
+    {
+        [Input("Contours", AutoValidate = false)]
+        public ISpread<ISpread<VContour>> FContours;
+
+        [Input("Winding Rule", DefaultEnumEntry = "EvenOdd", AutoValidate = false)]
+        public ISpread<WindingRule> FWinding;
+        
+        [Input("Tessellate", IsBang = true)]
+        public ISpread<bool> FTess;
+
+        [Output("Output")]
+        public ISpread<ISpread<Vector2D>> FOut;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (FTess.IsChanged)
+            {
+                FContours.Sync();
+                FWinding.Sync();
+                FOut.SliceCount = FContours.SliceCount;
+                for (int i = 0; i < FContours.SliceCount; i++)
+                {
+                    if (FTess[i])
+                    {
+                        var polygon = new VPolygon {
+                            Winding = FWinding[i]
+                        };
+                        for (int j = 0; j < FContours[i].SliceCount; j++)
+                        {
+                            if(FContours[i][j] != null)
+                                polygon.Contours.Add(FContours[i][j]);
+                        }
+                        polygon.Tessellate();
+                        FOut[i].SliceCount = polygon.Triangles.Length * 3;
+                        for (int j = 0; j < polygon.Triangles.Length; j++)
+                        {
+                            FOut[i][j * 3 + 0] = polygon[j][0];
+                            FOut[i][j * 3 + 1] = polygon[j][1];
+                            FOut[i][j * 3 + 2] = polygon[j][2];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/LibTessss/Types.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using LibTessDotNet;
+using VVVV.Utils.VMath;
+
+namespace LibTessss
+{
+    public class VContour
+    {
+        public ContourVertex[] Contour;
+        public ContourOrientation Orientation = ContourOrientation.Original;
+
+        public ContourVertex this[int i]
+        {
+            get { return Contour[i]; }
+            set { Contour[i] = value; }
+        }
+    }
+    public class VPolygon
+    {
+        public List<VContour> Contours { get; private set; } = new List<VContour>();
+        public Tess Tessellator { get; private set; } = new Tess();
+        public WindingRule Winding = WindingRule.EvenOdd;
+
+        public VTriangle[] Triangles { get; private set; }
+
+        public void Tessellate()
+        {
+            Tessellator.NoEmptyPolygons = true;
+            foreach (var c in Contours)
+            {
+                Tessellator.AddContour(c.Contour, c.Orientation);
+            }
+            Tessellator.Tessellate(Winding, ElementType.Polygons, 3);
+            Triangles = new VTriangle[Tessellator.ElementCount];
+            for (int i = 0; i < Tessellator.ElementCount; i++)
+            {
+                var v0 = Tessellator.Vertices[Tessellator.Elements[i * 3]];
+                var v1 = Tessellator.Vertices[Tessellator.Elements[i * 3 + 1]];
+                var v2 = Tessellator.Vertices[Tessellator.Elements[i * 3 + 2]];
+
+                Triangles[i] = new VTriangle
+                {
+                    ID = i,
+                    [0] = new Vector2D(v0.Position.X, v0.Position.Y),
+                    [1] = new Vector2D(v1.Position.X, v1.Position.Y),
+                    [2] = new Vector2D(v2.Position.X, v2.Position.Y)
+                };
+            }
+        }
+        public VTriangle this[int i] => Triangles[i];
+    }
+
+    public class VTriangle
+    {
+        public Vector2D[] Vertices { get; private set; } = new Vector2D[3];
+        public int ID;
+
+        public Vector2D this[int i]
+        {
+            get { return Vertices[i%3]; }
+            set { Vertices[i%3] = value; }
+        }
+    }
+}
